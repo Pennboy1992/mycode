ea9d0516336d079906d1eb948c7d25f169420000
Roman Penn <Pennboy1992@users.noreply.github.com>
2022-03-03 21:02:58 +0000

    writing custom code

---
+++ cust_if_script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def main():
+    played_games =int( input("How many Demon Souls/Dark Souls games have you played(including Elden Rings)? : ") )
+
+    if played_games > 5:
+        print("You're a true master chosen one go forth and bring back the embers")
+    elif played_games > 3:
+        print("I see you like a little bit of a challenge in your life")
+    else:
+        print("Train hard and you may find a few more victories")
+
+
+if __name__== "__main__":
+    main()
+
