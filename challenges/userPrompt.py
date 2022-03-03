4ce0dc1ecefccfbbc2bf189930a1b3b1a5c81df7
Roman Penn <Pennboy1992@users.noreply.github.com>
2022-03-03 17:10:29 +0000

    join and split

---
+++ challenges/userPrompt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+
+name = input("What is your name?")
+print(f"Nice to meet you, {name}!")
+saysomething = input("Did you have something you would like to say?")
+print(f"So you would like me to record: '{saysomething}'")
+userStatements = []
+userStatements.append(saysomething)
+
